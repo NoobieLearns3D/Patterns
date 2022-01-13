b97e4c783e9e5336d066de105f029f0493694dfe
Sanjay Ravishankar <aksam17.1237@gmail.com>
2022-01-14 00:47:51 +0530

    Star Pattern edit - 1

--- patternOne.cpp
+++ patternOne.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(void)
 {
-    for(int i=1;i<=5;i++)
+    for(int i=0;i<=5;i++)
 	{
-		for(int j=1;j<=i;j++)
-			cout<<j<<" ";
+		for(int j=0;j<=i;j++)
+			cout<<"*"<<" ";
 		cout<<endl;
 	}
 }
